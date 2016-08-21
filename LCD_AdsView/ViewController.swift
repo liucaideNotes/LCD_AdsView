092d7a9f1f973a141b0d55c76e4b651a88e30b5e
sifenzi <565726319@qq.com>
2016-08-21 13:39:34 +0800

    初创

---
+++ LCD_AdsView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LCD_AdsView
+//
+//  Created by 刘才德 on 16/8/21.
+//  Copyright © 2016年 sifenzi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
